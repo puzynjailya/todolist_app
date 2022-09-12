75302dc1ab1433471b31f99869f136d6b13d20b5
puzynjailya <puzynjailya@gmail.com>
2022-09-12 23:47:25 +0300

    settings update

--- app/ToDoList/settings.py
+++ app/ToDoList/settings.py
@@ -22,7 +22,6 @@ env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
 )
-env.read_env(env.str('ENV_PATH', '.env.deploy'))
 
 # Set the project base directory
 BASE_DIR = Path(__file__).resolve().parent.parent
