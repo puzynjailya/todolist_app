bf9ded2148a21f48d06db17150dbf11cdde3f130
puzynjailya <puzynjailya@gmail.com>
2022-11-06 01:25:28 +0300

    add __init__.py to app folder and some small updates

--- app/ToDoList/settings.py
+++ app/ToDoList/settings.py
@@ -161,7 +161,8 @@ AUTH_USER_MODEL = "core.User"
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 }
 
 SPECTACULAR_SETTINGS = {
